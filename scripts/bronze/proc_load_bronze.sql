5ca484260aa5647165dbfb382ff73da00b9bdddd
Muluken Bekele <mulebeki9.com@gmail.com>
2026-03-27 12:58:17 +0400

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -10,10 +10,10 @@ Script Purpose:
 
 Parameters:
     None. 
-	  This stored procedure does not accept any parameters or return any values.
+	This stored procedure does not accept any parameters or return any values.
 
 Usage Example:
-    EXEC bronze.load_bronze;   
+EXEC bronze.load_bronze;   
 =====================================================================================
 */
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
@@ -132,7 +132,7 @@ BEGIN
 	SET @batch_end_time = GETDATE();
 	PRINT '======================================================'
 	PRINT 'Loading Bronze Layer is Completed';
-	PRINT '  - Total Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) 
+	PRINT ' - Total Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) 
 	AS NVARCHAR) + ' seconds';
 	PRINT '======================================================'
 	END TRY
